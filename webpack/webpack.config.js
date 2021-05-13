e2672e63829910b49d77aa6c673ce7e5efe48881
yoeria <yoeri08@hotmail.com>
2021-05-13 17:39:21 +0200

    modified according to eslint rules

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -12,26 +12,26 @@ function generateConfig(name) {
       library: name,
       libraryTarget: 'umd'
     },
-    devtool: "source-map",
+    devtool: 'source-map',
     mode: 'production',
 
     resolve: {
       // Add '.ts' and '.tsx' as resolvable extensions.
-      extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js"],
+      extensions: ['.webpack.js', '.web.js', '.ts', '.tsx', '.js'],
       fallback: {
-        "crypto": require.resolve("crypto-browserify"),
-        "buffer": require.resolve("buffer/"),
-        "stream": require.resolve("stream-browserify")
+        'crypto': require.resolve('crypto-browserify'),
+        'buffer': require.resolve('buffer/'),
+        'stream': require.resolve('stream-browserify')
       }
     },
 
     module: {
       rules: [
         // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
-        { test: /\.tsx?$/, loader: "ts-loader" },
+        { test: /\.tsx?$/, loader: 'ts-loader' },
 
         // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-        { test: /\.js$/, loader: "source-map-loader" },
+        { test: /\.js$/, loader: 'source-map-loader' },
 
         {
           test: /\.m?js$/,
